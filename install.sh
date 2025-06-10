dfcb79be33ef3dc292a16c1c19d62745206adaa9
Bo Ren <boren@microsoft.com>
2025-06-10 15:03:15 -0700

    update env setup

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
-pip install tensordict
-pip install hydra-core
-pip install torchdata codetiming
+pip uninstall -y torch torchvision torchaudio flash-attn transformers
+pip install wandb==0.18.7
+pip install torch==2.6.0
+pip install math-verify==0.7.0
+pip install pylatexenc==2.10
+pip install transformers==4.51.3
+pip install deepspeed==0.16.7
+pip install torchdata==0.11.0
+pip install accelerate==1.6.0
+pip install codetiming==1.4.0
+pip install datasets==3.5.1
+pip install dill==0.4.0
+pip install hydra-core==1.3.2
+pip install numpy==1.26.4
+pip install pandas==2.2.3
+pip install peft==0.15.2
+pip install vllm==0.8.2
+pip install -U "pyarrow>=15.0.0"
+pip install pybind11==2.13.6
+pip install pylint==3.3.6
+pip install ray[default]==2.45.0
+pip install -U "tensordict<=0.6.2"
+pip install flash-attn==2.7.4.post1 --no-build-isolation
+pip install tensorboard==2.19.0
+pip install lm_eval==0.4.8
+pip install ray==2.36.1
 
-pip install vllm==0.8.2 --no-deps
+
