6f3f0a8007baa221636be3dc0c2a5468c71f7a90
Bo Ren <boren@microsoft.com>
2025-06-18 16:02:13 -0700

    fix move data

--- move_data.sh
+++ move_data.sh
@@ -1,5 +1,6 @@
 
 DATA_DIR=/root/data
-bbb cpr  az://orngwus2cresco/data/boren/data/gsm8k ${DATA_DIR}/gsm8k
-# bbb cpr  az://orngwus2cresco/data/boren/data/ckp/Qwen ${DATA_DIR}/ckp/Qwen
-bbb cpr  az://orngwus2cresco/data/boren/data/ckp/hf_models ${DATA_DIR}/ckp/hf_models
+
+bbb cpr --concurrency 64  az://orngscuscresco/data/boren/data/gsm8k ${DATA_DIR}/gsm8k
+# bbb cpr --concurrency 64  az://orngscuscresco/data/boren/data/ckp/Qwen ${DATA_DIR}/ckp/Qwen
+bbb cpr --concurrency 64  az://orngscuscresco/data/boren/data/ckp/hf_models ${DATA_DIR}/ckp/hf_models
