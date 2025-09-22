f31c10dd2b540e34ae3e6905e6a125fc3312aeb5
Bo Ren <boren@microsoft.com>
2025-09-22 18:50:52 +0000

    use docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,17 @@
 
 # Docker image
+set -x
 # reference: https://verl.readthedocs.io/en/latest/start/install.html
 image=verlai/verl:app-verl0.5-vllm0.9.1-mcore0.12.2-te2.2
 name=boren_verl
-# podman pull $image
-# podman create --runtime=nvidia --gpus all --net=host --shm-size="10g" --cap-add=SYS_ADMIN -v /home/boren:/home/boren -v /datablob1:/datablob1 --name ${name} ${image} sleep infinity
-# podman start ${name}
-# podman exec -it ${name} bash
+# docker pull $image
+# docker create --runtime=nvidia --gpus all --net=host --shm-size="10g" --cap-add=SYS_ADMIN -v /home/boren:/home/boren -v /datablob1:/datablob1 --name ${name} ${image} sleep infinity
+# docker start ${name}
+# docker exec -it ${name} bash
 #
-podman stop ${name} && podman rm ${name}
-podman run --rm -d --gpus all --ipc host -v /datablob1:/datablob1 -v  /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name ${name} ${image} tail -f /dev/null
-podman exec -it ${name} bash
+docker stop ${name} && docker rm ${name}
+docker run --rm -d --gpus all --ipc host -v /datablob1:/datablob1 -v  /home/boren:/home/boren -v /mnt:/mnt -v /mnt2:/mnt2 --name ${name} ${image} tail -f /dev/null
+docker exec -it ${name} bash
 
 # git clone https://github.com/volcengine/verl && cd verl
 # pip3 install -e .[vllm]
