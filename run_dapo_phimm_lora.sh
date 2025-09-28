ae149f46bd17916bd22e31ad2087aefbe5d6f848
Bo Ren <boren@microsoft.com>
2025-09-28 22:27:36 +0000

    update bash

--- run_dapo_phimm_lora.sh
+++ run_dapo_phimm_lora.sh
@@ -2,16 +2,16 @@
 set -xeuo pipefail
 
 NUM_GPUS=${NUM_GPUS:-1}
-HOME=/home/boren
-MODEL_PATH=/home/boren/data/ckp/hf_models/phi4_mm_bias_merged
+HOME=/root
+MODEL_PATH=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
 
-# train_data="${HOME}/data/parquet/ls_clean_sc1k_fn1.parquet"
-train_data="${HOME}/data/parquet/ls_clean_sc1k_fn1_h100.parquet"
-test_data="${HOME}/data/parquet/ls_clean_sc1k_fn1_h100.parquet"
+train_data=${HOME}/data/parquet/ls_sc1k_fn1_remote.parquet
+test_data=${HOME}/data/parquet/ls_sc1k_fn1_h100_remote.parquet
 
 
 python3 -m recipe.dapo.main_dapo \
 --config-name dapo_phimm_ls_lora \
 data.train_files="${train_data}" \
 data.val_files="${test_data}" \
+trainer.n_gpus_per_node="${NUM_GPUS}" \
 actor_rollout_ref.model.path="${MODEL_PATH}"
