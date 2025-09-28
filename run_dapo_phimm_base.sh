514b341f3abba5f7b40b17f8f2b2a564ce6ea244
Bo Ren <boren@microsoft.com>
2025-09-28 22:54:09 +0000

    udpate script

--- run_dapo_phimm_base.sh
+++ run_dapo_phimm_base.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-NUM_GPUS=${NUM_GPUS:-1}
 HOME=/root
 MODEL_PATH=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
 
@@ -13,5 +12,6 @@ python3 -m recipe.dapo.main_dapo \
 --config-name dapo_phimm_base \
 data.train_files="${train_data}" \
 data.val_files="${test_data}" \
-trainer.n_gpus_per_node="${NUM_GPUS}" \
 actor_rollout_ref.model.path="${MODEL_PATH}"
+
+# trainer.n_gpus_per_node="${NUM_GPUS}" \
