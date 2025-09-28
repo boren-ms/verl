b90a2e6a5b43c1e1b9742bbe9213e8cc20ea4fa2
Bo Ren <boren@microsoft.com>
2025-09-28 23:00:48 +0000

    update script

--- run_dapo_phimm_lora.sh
+++ run_dapo_phimm_lora.sh
@@ -7,11 +7,12 @@ MODEL_PATH=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
 train_data=${HOME}/data/parquet/ls_sc1k_fn1_remote.parquet
 test_data=${HOME}/data/parquet/ls_sc1k_fn1_h100_remote.parquet
 
+config_name=dapo_phimm_lora
 
 python3 -m recipe.dapo.main_dapo \
---config-name dapo_phimm_lora \
+--config-name ${config_name} \
 data.train_files="${train_data}" \
 data.val_files="${test_data}" \
-actor_rollout_ref.model.path="${MODEL_PATH}"
+actor_rollout_ref.model.path="${MODEL_PATH}" 2>&1 | tee ${config_name}.log
 
 # trainer.n_gpus_per_node="${NUM_GPUS}"
