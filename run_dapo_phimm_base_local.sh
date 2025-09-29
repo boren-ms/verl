86eff1a6992d631617b909dc7a1378b7bfa090c3
Bo Ren <boren@microsoft.com>
2025-09-29 16:57:19 +0000

    add local debug script

---
+++ run_dapo_phimm_base_local.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+HOME=/home/boren
+MODEL_PATH=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
+
+train_data=${HOME}/data/parquet/ls_clean_sc1k_fn1.parquet
+test_data=${HOME}/data/parquet/ls_clean_sc1k_fn1_h100.parquet
+
+config_name=dapo_phimm_base
+
+python3 -m recipe.dapo.main_dapo \
+--config-name ${config_name} \
+data.train_files="${train_data}" \
+data.val_files="${test_data}" \
+trainer.n_gpus_per_node=1 \
+actor_rollout_ref.model.path="${MODEL_PATH}" 2>&1 | tee ${config_name}.log
