1f6b7493ebb2048094575a3c90bd451bba296797
Bo Ren <boren@microsoft.com>
2025-10-01 02:31:35 +0000

    min lr rate 0.1

---
+++ run_dapo_phimm_lora_v1a.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+HOME=/root
+model_path=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
+
+train_data=${HOME}/data/parquet/ls_sc1k_fn1_remote.parquet
+test_data=${HOME}/data/parquet/ls_sc1k_fn1_h100_remote.parquet
+
+python3 -m recipe.dapo.main_dapo \
+--config-name dapo_phimm_lora \
+data.train_files="${train_data}" \
+data.val_files="${test_data}" \
+data.train_batch_size=200 \
+data.gen_batch_size=300 \
+actor_rollout_ref.rollout.n=8 \
++data.eval_num_examine=5 \
++data.train_num_examine=1 \
+actor_rollout_ref.model.path=${model_path} \
+actor_rollout_ref.actor.ppo_mini_batch_size=1600 \
+actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=200 \
+actor_rollout_ref.actor.entropy_coeff=0 \
+actor_rollout_ref.actor.optim.min_lr_ratio=0.1 \
+actor_rollout_ref.model.enable_gradient_checkpointing=True \
+actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=200 \
+actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=200 \
+actor_rollout_ref.ref.fsdp_config.param_offload=True \
+trainer.total_epochs=1  2>&1 | tee run_dapo_phimm_lora_v1.log
+
+# trainer.n_gpus_per_node="${NUM_GPUS}"
