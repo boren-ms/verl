455a23931821337938522cc6310386b9fa82ca2d
Bo Ren <boren@microsoft.com>
2025-10-03 22:38:58 +0000

    update test script

---
+++ run_remote_test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+config_name=dapo_prod_fy22_bs8_n16_rep
+
+
+export DATA_PATH=az://orngcresco/data/boren/data
+export OUTPUT_PATH=/root/outputs/${config_name}
+
+python3 -m recipe.phimm.main_dapo \
+--config-name ${config_name} \
+actor_rollout_ref.rollout.n=4 \
+trainer.n_gpus_per_node=1 \
+trainer.logger=console \
+trainer.default_local_dir="${OUTPUT_PATH}" 2>&1 | tee ${config_name}.log
