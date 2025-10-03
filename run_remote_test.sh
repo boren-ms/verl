8fd8b86f3f37a2bafd1c70e447992cdd2bc7cc12
Bo Ren <boren@microsoft.com>
2025-10-03 23:43:15 +0000

    fix

--- run_remote_test.sh
+++ run_remote_test.sh
@@ -10,6 +10,7 @@ export OUTPUT_PATH=/root/outputs/${config_name}
 python3 -m recipe.phimm.main_dapo \
 --config-name ${config_name} \
 actor_rollout_ref.rollout.n=4 \
+actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
 trainer.n_gpus_per_node=1 \
 trainer.logger=console \
 trainer.default_local_dir="${OUTPUT_PATH}" 2>&1 | tee ${config_name}.log
