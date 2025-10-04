a0bfda04096e6591cf5d329e85e281b81b2fcc21
Bo Ren <boren@microsoft.com>
2025-10-04 19:57:26 +0000

    revert test script

--- run_local_test.sh
+++ run_local_test.sh
@@ -1,29 +1,19 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-HOME=/root
-# export DATA_PATH=az://${DATA_STORAGE}/data/boren/data
-export DATA_PATH="az://orngcresco/data/boren/data"
-export MODEL_PATH=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
+# export MODEL_PATH=/home/boren/data/ckp/hf_models/phi4_mm_bias_merged
+export MODEL_PATH=/home/boren/data/ckp/hf_models/Phi-4-multimodal-instruct
+# export MODEL_PATH=/home/boren/data/ckp/hf_models/phi-libri_ft_m1000_p8_new-QpHq/5000_hf
+# export MODEL_PATH=/home/boren/data/ckp/hf_models/phi4-7b-fast-api-s2-final-v4
+export DATA_PATH=/home # used in yamls
 
-batch_size=128
-config_name=dapo_phimm_lora_ls
+config_name=dapo_local_test
+
 python3 -m recipe.phimm.main_asr_dapo \
 --config-name ${config_name} \
-data.train_batch_size=${batch_size} \
-data.gen_batch_size=$((batch_size * 3 / 2)) \
-actor_rollout_ref.rollout.n=8 \
-actor_rollout_ref.model.path=${MODEL_PATH} \
-actor_rollout_ref.actor.ppo_mini_batch_size=$((batch_size * 8)) \
-actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${batch_size} \
-actor_rollout_ref.actor.entropy_coeff=0 \
-actor_rollout_ref.actor.optim.min_lr_ratio=0.1 \
-actor_rollout_ref.model.enable_gradient_checkpointing=True \
-actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${batch_size} \
-actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
-actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${batch_size} \
-actor_rollout_ref.ref.fsdp_config.param_offload=True \
-trainer.experiment_name=${config_name} \
-trainer.total_epochs=1  2>&1 | tee ${config_name}.log
-
-# trainer.n_gpus_per_node="${NUM_GPUS}"
+data.train_batch_size=4 \
+data.gen_batch_size=4 \
+actor_rollout_ref.rollout.n=4 \
+trainer.n_gpus_per_node=1 \
+trainer.logger=console \
+actor_rollout_ref.model.path="${MODEL_PATH}" 2>&1 | tee ${config_name}.log
--- run_remote_test.sh
+++ run_remote_test.sh
@@ -1,29 +1,16 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-HOME=/root
-# export DATA_PATH=az://${DATA_STORAGE}/data/boren/data
-export DATA_PATH="az://orngcresco/data/boren/data"
-export MODEL_PATH=${HOME}/data/ckp/hf_models/phi4_mm_bias_merged
+config_name=dapo_prod_fy22_bs8_n16_rep
 
-batch_size=128
-config_name=dapo_phimm_lora_ls
+
+export DATA_PATH=az://orngcresco/data
+export OUTPUT_PATH=/root/outputs/${config_name}
+
 python3 -m recipe.phimm.main_asr_dapo \
 --config-name ${config_name} \
-data.train_batch_size=${batch_size} \
-data.gen_batch_size=$((batch_size * 3 / 2)) \
-actor_rollout_ref.rollout.n=8 \
-actor_rollout_ref.model.path=${MODEL_PATH} \
-actor_rollout_ref.actor.ppo_mini_batch_size=$((batch_size * 8)) \
-actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=${batch_size} \
-actor_rollout_ref.actor.entropy_coeff=0 \
-actor_rollout_ref.actor.optim.min_lr_ratio=0.1 \
-actor_rollout_ref.model.enable_gradient_checkpointing=True \
-actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=${batch_size} \
-actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
-actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=${batch_size} \
-actor_rollout_ref.ref.fsdp_config.param_offload=True \
-trainer.experiment_name=${config_name} \
-trainer.total_epochs=1  2>&1 | tee ${config_name}.log
-
-# trainer.n_gpus_per_node="${NUM_GPUS}"
+actor_rollout_ref.rollout.n=4 \
+actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+trainer.n_gpus_per_node=1 \
+trainer.logger=console \
+trainer.default_local_dir="${OUTPUT_PATH}" 2>&1 | tee ${config_name}.log
