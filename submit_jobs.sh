7aa07386f88421417ed395077abee5f66f93663e
Bo Ren <boren@microsoft.com>
2025-10-05 03:43:09 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64.yaml & # background job
+# bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64.yaml & # background job
+bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_prod_fy22_bs8_n16_rep.yaml & # background job
