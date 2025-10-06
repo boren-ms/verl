94d5a40ec861659a1de6bbe487a257227d88420f
Bo Ren <boren@microsoft.com>
2025-10-06 22:02:04 +0000

    support val only

--- quick_run.sh
+++ quick_run.sh
@@ -2,6 +2,7 @@
 set -xeuo pipefail
 
 config_file=$1
+val_only=${2:-"false"}
 
 config_base=$(basename "$config_file")
 config_name=${config_base%.*}
@@ -17,6 +18,7 @@ echo "[INFO] Running ${config_name} ..."
 ray job submit --working-dir=${cwd} --no-wait -- \
 python3 -m recipe.phimm.main_asr_dapo \
 --config-name "${config_name}" \
+trainer.val_only="${val_only}" \
 trainer.experiment_name="${config_name}" \
 2>&1 | tee "${config_name}.log"
 
