a8896705cdf7eacb8d76f4e37cf5f077323a7d13
Bo Ren <boren@microsoft.com>
2025-10-06 01:13:32 +0000

    update ray job

--- ray_job.py
+++ ray_job.py
@@ -2,11 +2,23 @@
 # %%
 from ray.job_submission import JobSubmissionClient
 import fire
+import ray
+import socket
+
+
+def ray_url(address="auto"):
+    """Get the Ray dashboard URL."""
+    if not ray.is_initialized():
+        ray.init(address=address)
+    url = ray._private.worker.global_worker.node.address_info["webui_url"]
+    return f"http://{url}"
 
 
 class RayJob:
     def __init__(self, address="auto"):
         self.client = JobSubmissionClient(address=address)
+        self.url = ray_url(address)
+        self.host = socket.gethostname()
 
     def _jobs(self, status=None, submit=False):
         jobs = self.client.list_jobs()
@@ -21,8 +33,11 @@ class RayJob:
         """List running Ray jobs."""
         status = "RUNNING" if running else None
         jobs = self._jobs(status=status, submit=submit)
-        for job in jobs:
-            print(job.entrypoint)
+        print("\nHost:", self.host, "Ray:", self.url)
+        for i, job in enumerate(jobs):
+            if "ray_job.py" in job.entrypoint:
+                continue
+            print(f"[{i}]", job.entrypoint)
 
     def cleanup(self, command=None):
         """Clean up all stopped Ray jobs."""
