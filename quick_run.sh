5bec774fb0fb1606da79fc117db2f89522944aa2
Bo Ren <boren@microsoft.com>
2025-10-06 23:06:49 +0000

    fix issues

--- quick_run.sh
+++ quick_run.sh
@@ -2,7 +2,6 @@
 set -xeuo pipefail
 
 config_file=$1
-val_only=${2:-"false"}
 
 config_base=$(basename "$config_file")
 config_name=${config_base%.*}
@@ -18,7 +17,6 @@ echo "[INFO] Running ${config_name} ..."
 ray job submit --working-dir=${cwd} --no-wait -- \
 python3 -m recipe.phimm.main_asr_dapo \
 --config-name "${config_name}" \
-trainer.val_only="${val_only}" \
 trainer.experiment_name="${config_name}" \
 2>&1 | tee "${config_name}.log"
 
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# # bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml & # background job
+bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml & # background job
 # bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bias_bs64_2k.yaml & # background job
 # bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.yaml &
 # bash submit_job.sh h1-n2-hpe4 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml &
@@ -15,6 +15,6 @@
 # bash submit_job.sh h-n2-wus2 recipe/phimm/config/eval_phimm.yaml & # background job
 # bash submit_job.sh h-n2-wus2 recipe/phimm/config/eval_phimm_bias_sft.yaml & # background job
 # bash submit_job.sh h-n2-wus2 recipe/phimm/config/eval_phimm_7b.yaml & # background job
-bash submit_job.sh dev1-n1-wus2 recipe/phimm/config/eval_phimm_rlbr.yaml & # background job
+# bash submit_job.sh dev1-n1-wus2 recipe/phimm/config/eval_phimm_rlbr.yaml & # background job
 # bash submit_job.sh h-n2-wus2 recipe/phimm/config/grpo_ls_bias_bs64_rep_2k.yaml & # background job
 
