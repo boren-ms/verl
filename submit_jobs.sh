6662c9ece1724813f6dc52096273c7c7779a1f27
Bo Ren <boren@microsoft.com>
2025-10-06 06:11:20 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # bash submit_job.sh h-n1-uks7 recipe/phimm/config/dapo_ls_bias_bs64_rep_e2.yaml & # background job
-# bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml & # background job
-bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bias_bs64_2k.yaml & # background job
-bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_prod_fy22_bias_bs64_rep.yaml & # background job
+bash submit_job.sh h-n1-uks7 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml & # background job
+bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml & # background job
+# bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bias_bs64_2k.yaml & # background job
+# bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_prod_fy22_bias_bs64_rep.yaml & # background job
 # bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.yaml &
 # bash submit_job.sh h1-n2-hpe4 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml &
 # bash submit_job.sh h1-n2-hpe4 recipe/phimm/config/dapo_prod_fy22_bs64_rep_me10_2k.yaml &
