61b1b76d475010df922633238b658bdc7aeba298
Bo Ren <boren@microsoft.com>
2025-10-07 05:43:25 +0000

    udpate jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml & # background job
+bash submit_job.sh h-n2-wus2  recipe/phimm/config/dapo_ls_bs64_2k.yaml & # background job
 # bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bias_bs64_2k.yaml & # background job
-# bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.yaml &
+bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.yaml &
 # bash submit_job.sh h1-n2-hpe4 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml &
 # # bash submit_job.sh h1-n2-wus2 recipe/phimm/config/dapo_ls_bias_bs64_n16_rep_2k.yaml &
 # bash submit_job.sh h1-n2-wus2 recipe/phimm/config/dapo_ls_bias_bs64_rep_e2.yaml & # background job
@@ -12,10 +12,10 @@ bash submit_job.sh l-n1-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml & # backgr
 # bash submit_job.sh dev-n1-uks7 recipe/phimm/config/dapo_prod_fy22_bs64_2k.yaml &
 # bash submit_job.sh dev-n1-wus2 recipe/phimm/config/dapo_7b_prod_fy22_bs64_rep_2k.yaml &
 # bash submit_job.sh h-n1-uks7 recipe/phimm/config/dapo_rare_fy32q2_bs64_rep.yaml & # background job
-bash submit_job.sh h-n2-wus2 recipe/phimm/config/grpo_ls_bias_bs64_rep_2k.yaml & # background job
-bash submit_job.sh h-n2-wus2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml & # background job
-# bash submit_job.sh dev1-n1-wus2 recipe/phimm/config/eval_phimm.yaml & # background job
-# bash submit_job.sh dev1-n1-wus2 recipe/phimm/config/eval_phimm_bias_sft.yaml & # background job
-# bash submit_job.sh dev1-n1-wus2 recipe/phimm/config/eval_phimm_7b.yaml & # background job
-# bash submit_job.sh dev1-n1-wus2 recipe/phimm/config/eval_phimm_rlbr.yaml & # background job
+# bash submit_job.sh h-n2-wus2 recipe/phimm/config/grpo_ls_bias_bs64_rep_2k.yaml & # background job
+bash submit_job.sh h-n2-wus2  recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml & # background job
+bash submit_job.sh l-n2-hpe2 recipe/phimm/config/eval_phimm.yaml & # background job
+bash submit_job.sh l-n2-hpe2 recipe/phimm/config/eval_phimm_bias_sft.yaml & # background job
+bash submit_job.sh l-n2-hpe2 recipe/phimm/config/eval_phimm_7b.yaml & # background job
+bash submit_job.sh l-n2-hpe2 recipe/phimm/config/eval_phimm_rlbr.yaml & # background job
 
