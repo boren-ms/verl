c7a04973fbb7433d3ca4be2164a5bbad39ddbbcb
Bo Ren <boren@microsoft.com>
2025-10-08 01:19:09 +0000

    update yamls

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -2,7 +2,7 @@
 
 # bash submit_job.sh l-n1-uks7  recipe/phimm/config/dapo_ls_bs64_2k.yaml
 # bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bias_bs64_2k.yaml
-bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.yaml &
+# bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.yaml
 # bash submit_job.sh h1-n2-hpe4 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml
 # # bash submit_job.sh h1-n2-wus2 recipe/phimm/config/dapo_ls_bias_bs64_n16_rep_2k.yaml
 # bash submit_job.sh h1-n2-wus2 recipe/phimm/config/dapo_ls_bias_bs64_rep_e2.yaml
@@ -12,10 +12,12 @@ bash submit_job.sh l-n1-hpe2 recipe/phimm/config/dapo_ls_bias_bs64_rep_2k_n16.ya
 # bash submit_job.sh dev-n1-uks7 recipe/phimm/config/dapo_prod_fy22_bs64_2k.yaml
 # bash submit_job.sh dev-n1-wus2 recipe/phimm/config/dapo_7b_prod_fy22_bs64_rep_2k.yaml
 # bash submit_job.sh h-n1-uks7 recipe/phimm/config/dapo_rare_fy32q2_bs64_rep.yaml
+# bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bs64_2k_v1.yaml
 # bash submit_job.sh h-n2-wus2 recipe/phimm/config/grpo_ls_bias_bs64_rep_2k.yaml
 # bash submit_job.sh h-n2-wus2  recipe/phimm/config/dapo_ls_bias_bs64_rep_2k.yaml
-# bash submit_job.sh h-n2-uks7 recipe/phimm/config/eval_phimm.yaml
-# bash submit_job.sh h-n2-uks7 recipe/phimm/config/eval_phimm_bias_sft.yaml
+# bash submit_job.sh h-n2-uks7 recipe/phimm/config/dapo_ls_bs64_2k.yaml
+bash submit_job.sh l-n1-uks7 recipe/phimm/config/eval_phimm.yaml
 # bash submit_job.sh l-n2-hpe2 recipe/phimm/config/eval_phimm_7b.yaml
 # bash submit_job.sh l-n2-hpe2 recipe/phimm/config/eval_phimm_rlbr.yaml
+# bash submit_job.sh l-n1-uks7 recipe/phimm/config/eval_phimm_bias_sft.yaml
 
