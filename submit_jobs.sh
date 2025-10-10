391cd9febeb12eb7ee8df6c8c635db30ec2b52e9
Bo Ren <boren@microsoft.com>
2025-10-10 20:24:29 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -23,10 +23,13 @@
 # bash submit_job.sh h1-n2-hpe4 dapo_ls_bias_bs64_rep_2k true
 # bash submit_job.sh h1-n2-hpe4 grpo_ls_bias_bs64_rep_2k
 
-# bash submit_job.sh l-n1-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5
-bash submit_job.sh l-n1-hpe2 grpo_ls_bias_bs64_rep_2k_gt_t12
-# bash submit_job.sh h-n2-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5
+bash submit_job.sh l-n1-uks7 grpo_ls_bias_bs64_rep_2k_t12
+bash submit_job.sh l1-n1-hpe2 grpo_ls_bias_bs64_rep_2k_gt_t12
+bash submit_job.sh h-n2-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5
 # bash submit_job.sh l-n1-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_b8 true
 # bash submit_job.sh l-n1-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_b16
 # bash submit_job.sh h1-n2-wus2 dapo_ls_bias_bs64_rep_e2 true
-# bash submit_job.sh h-n2-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_n16
+bash submit_job.sh h1-n2-wus2 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_n16
+
+
+# bash submit_job.sh h-n2-uks7 dapo_ls_bs64_2k_eb
