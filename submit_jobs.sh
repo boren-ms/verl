ed10ab232401d92dafe16823271518f03960631d
Bo Ren <boren@microsoft.com>
2025-10-12 23:42:21 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -43,10 +43,12 @@
 # bash submit_job.sh dev-n1-uks7 grpo_prod_fy22_bs64_gt_tis5_2k
 
 # bash submit_job.sh l-n1-uks7 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_raw true
-bash submit_job.sh l-n1-uks7 grpo_ls_eb_bs64_rep_2k_gt_t12_tis5_n2
-bash submit_job.sh h1-n2-hpe4 grpo_ls_eb_bs64_rep_2k_gt_t12_tis5_n8
+# bash submit_job.sh l-n1-uks7 grpo_ls_eb_bs64_rep_2k_gt_t12_tis5_n2
+# bash submit_job.sh h1-n2-hpe4 grpo_ls_eb_bs64_rep_2k_gt_t12_tis5_n8
 # bash submit_job.sh l-n1-hpe2 grpo_ls_bs64_gt_t12_tis5_2k
 
 # bash submit_job.sh dev1-n1-wus2 grpo_prod_fy22_7b_bs64_rep_gt_t12_tis5_2k
 # bash submit_job.sh dev-n1-wus2 grpo_ls_bias_bs64_rep_2k_gt_t12_b16
 # bash submit_job.sh dev-n1-wus2 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_sm
+
+bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_bs64_gt_t12_tis5_eb_n8_2k
