ecb34e7556ba5630ed88f913d012c7c63ee15c29
Bo Ren <boren@microsoft.com>
2025-10-13 21:42:41 +0000

    update jobs

--- fetch_result.sh
+++ fetch_result.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-python ./wandb_result.py --metric val-aux search 'eval_phimm'
+# python ./wandb_result.py --metric val-aux search 'eval_phimm'
+python ./wandb_result.py --metric val-aux search 'ls_bias_bs64'
