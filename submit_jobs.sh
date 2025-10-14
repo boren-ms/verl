a278ba6cbfd56a43b7e0391bd4a999443bd0018d
Bo Ren <boren@microsoft.com>
2025-10-14 16:45:46 +0000

    add configs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -61,6 +61,9 @@
 # bash submit_job.sh l-n1-hpe2 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_n2x4
 # bash submit_job.sh h1-n2-wus2 dapo_prod_fy22_bs64_rep_2k true
 # bash submit_job.sh h1-n2-wus2 dapo_prod_fy22_bs64_gt_tis5_me10_n16_2k
-bash submit_job.sh l-n1-uks7 grpo_ls_eb_bs64_rep_2k_gt_t12_tis5_n2
+# bash submit_job.sh l-n1-uks7 grpo_ls_eb_bs64_rep_2k_gt_t12_tis5_n2
 # bash submit_job.sh h-n2-hpe4 grpo_ls_eb_bs64_t12_tis5_n2_4k true
 # bash submit_job.sh h-n2-hpe4 grpo_ls_rare05_gt_t12_tis5_n8_2k
+
+bash submit_job.sh dev1-n1-wus2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n8_2k
+bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n8_me10_2k
