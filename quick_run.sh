2545b413876f37ef9a4d78392823f75fabf82434
Bo Ren <boren@microsoft.com>
2025-10-17 02:10:42 +0000

    support run different module

--- quick_run.sh
+++ quick_run.sh
@@ -10,6 +10,13 @@ cwd="$(dirname $(readlink -f $0))"
 echo "Current working directory: ${cwd}"
 pushd "$cwd" > /dev/null
 
+
+module="recipe.phimm.main_asr_dapo"
+
+if [[ "$config_name" == gen_* ]]; then
+    module="recipe.phimm.main_asr_gen"
+fi
+
 # bash quick_install.sh # prepare on local node only
 echo "[INFO] Preparing environment ..."
 python3 ray_tool.py prepare_env # prepare on all ray nodes
@@ -17,7 +24,7 @@ python3 ray_tool.py prepare_env # prepare on all ray nodes
 echo "[INFO] Running ${config_name} ..."
 ray job submit --working-dir="${cwd}"  \
 --no-wait -- \
-python3 -m recipe.phimm.main_asr_dapo \
+python3 -m ${module} \
 --config-name "${config_name}" \
 trainer.experiment_name="${config_name}" \
 2>&1 | tee "${config_name}.log"
