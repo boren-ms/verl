0c66de2f6046a2a7363914bd6869fbc2acf00fa1
Bo Ren <boren@microsoft.com>
2025-10-19 00:45:09 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -13,7 +13,7 @@
 
 
 # bash submit_job.sh dev-n1-uks7 dapo_prod_fy22_bs64_2k
-# bash submit_job.sh h-n1-uks7 dapo_rare_fy32q2_bs64_rep
+# bash submit_job.sh h-n1-uks7 dapo_rare_fy23q2_bs64_rep
 
 # bash submit_job.sh h-n2-wus2  grpo_ls_bias_bs64_rep_2k true # dryrun, will cleanup
 # bash submit_job.sh h-n2-wus2  dapo_ls_bias_bs64_rep_2k_n16
@@ -36,7 +36,7 @@
 # bash submit_job.sh h1-n2-hpe4 grpo_ls_bias_bs64_rep_2k true
 # bash submit_job.sh h-n2-wus2 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_dr
 
-# bash submit_job.sh h-n1-uks7 dapo_rare_fy32q2_bs64_rep  true
+# bash submit_job.sh h-n1-uks7 dapo_rare_fy23q2_bs64_rep  true
 # bash submit_job.sh h-n1-uks7 grpo_prod_fy22_bs64_rep_gt_tis5_2k
 # bash submit_job.sh dev-n1-uks7 dapo_prod_fy22_bs64_2k true
 # bash submit_job.sh dev-n1-uks7 grpo_prod_fy22_bs64_gt_tis5_2k
@@ -71,14 +71,19 @@
 # bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2_2k
 # bash submit_job.sh h-n2-hpe4 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_n2x4
 
-# bash submit_job.sh l1-n1-hpe2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_2k
 # bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2_8k
 
 # bash submit_job.sh dev-n1-wus2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n8_2k
 # bash submit_job.sh h-n2-wus2 dapo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4
 
-# bash submit_job.sh l-n1-hpe2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_a0_2k
-
-# bash submit_job.sh dev1-n1-wus2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_2k_simple
-bash submit_job.sh h-n2-hpe4 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_n2x4 true
-bash submit_job.sh h-n2-hpe4 gen_prod_fy22_phi4_7b_wer_01_20_err_1_20
+# bash submit_job.sh dev-n1-hpe2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_2k
+# bash submit_job.sh h1-n2-wus2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_2k_simple
+# bash submit_job.sh l-n1-hpe2 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_2k
+# bash submit_job.sh h-n2-hpe4 grpo_ls_bias_bs64_rep_2k_gt_t12_tis5_n2x4 true
+# bash submit_job.sh h1-n2-wus2 gen_prod_fy22_phi4_7b_wer_20
+# bash submit_job.sh h-n2-hpe4 gen_prod_fy22_phi4_7b_wer_01_20
+# bash submit_job.sh h1-n2-hpe4 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2x4_a0_2k
+# bash submit_job.sh h-n2-uks7 grpo_bias_7b_prod_fy22_rep_gt_t12_tis5_n2x4_2k
+# bash submit_job.sh h-n2-wus2 gen_sr_fy23q2_phi4_7b_wer_01_20
+bash submit_job.sh h-n2-uks7 grpo_bias_7b_prod_fy22_rep_gt_t12_tis5_n2x4_2k 
+bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare05_gt_t12_tis5_n2_8k 
