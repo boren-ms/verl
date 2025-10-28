9512eb704df748fc1527f572d3fa66480f9d0995
Bo Ren <boren@microsoft.com>
2025-10-28 21:29:28 +0000

    fix submit job

--- submit_job.sh
+++ submit_job.sh
@@ -22,14 +22,14 @@ if [ "$cleanup" == "true" ]; then
     echo "cleaning up old ${config_name} on $job_name"
     rcall-brix ssh "$job_name" "bash -lc \"python /root/code/verl/ray_job.py cleanup ${config_name}\""
 fi
+log_dir=logs/$job_name
+mkdir -p "$log_dir"
 
 if [ "$dry_run" == "true" ]; then
     echo "Dry run"
-    echo rcall-brix ssh  "$job_name" "bash -l /root/code/verl/quick_run.sh $config_file"  | tee "${job_name}_${config_name}.log"
+    echo rcall-brix ssh  "$job_name" "bash -l /root/code/verl/quick_run.sh $config_file"  | tee "${log_dir}/${config_name}.log"
     exit 0
 fi
 
-log_dir=logs/$job_name
-mkdir -p "$log_dir"
 
 rcall-brix ssh  "$job_name" "bash -l /root/code/verl/quick_run.sh $config_file" | tee "${log_dir}/${config_name}.log"
