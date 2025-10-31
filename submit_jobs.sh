73a165fedf2ec6afecf8cfcf03d9e479166fb486
Bo Ren <boren@microsoft.com>
2025-10-31 17:21:10 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -23,8 +23,9 @@
 # bash submit_job.sh h1-n2-hpe4 grpo_gpt_entity_v0_7b_n16_2k_remax
 # bash submit_job.sh l-n1-hpe2     grpo_gpt_entity_v0_7b_n4_2k_remax
 # bash submit_job.sh h-n2-wus2     grpo_gpt_entity_sr_fy22_rare01_7b_n4_4k_remax
-bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_t06 true
-bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full
+# bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full true
+# bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_t06
+bash submit_job.sh h-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full
 
 # bash submit_job.sh h-n2-uks7 dapo_tts_entity_7b_gt_t12_tis5_n16_2k_mw10 true
 # bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_bs128_t06
