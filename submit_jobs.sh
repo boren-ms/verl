bf26013403abe6a0e5a32199934effa292a0ed3b
Bo Ren <boren@microsoft.com>
2025-11-01 19:41:22 +0000

    update configs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -25,7 +25,15 @@
 # bash submit_job.sh h-n2-wus2     grpo_gpt_entity_sr_fy22_rare01_7b_n4_4k_remax
 # bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full true
 # bash submit_job.sh h-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_t06
-bash submit_job.sh h-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full
+# bash submit_job.sh h-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full
+# bash submit_job.sh h-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_full
+# bash submit_job.sh h1-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_2k_kl1_gtref
+# bash submit_job.sh dev1-n1-hpe5 grpo_prod_fy22_7b_rare01_n8_mw05_2k_kl01
+# bash submit_job.sh l-n1-hpe5 grpo_prod_fy22_7b_rare01_n8_mw05_2k_kl01_gtref
+# bash submit_job.sh l1-n1-hpe2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_fp16
+# bash submit_job.sh h1-n2-uks7 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_fp16a
+# bash submit_job.sh dev-n1-uks7 grpo_gpt_entity_v0_7b_n4_2k_remax_dapo_t06
+bash submit_job.sh h-n2-hpe4 grpo_gpt_entity_v0_7b_n4_2k_remax_t06
 
 # bash submit_job.sh h-n2-uks7 dapo_tts_entity_7b_gt_t12_tis5_n16_2k_mw10 true
 # bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_bs128_t06
