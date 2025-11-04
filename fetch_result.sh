993de5a262e2fd7722b54ce4f34ee244aec347b6
Bo Ren <boren@microsoft.com>
2025-11-04 02:08:05 +0000

    update jobs

--- fetch_result.sh
+++ fetch_result.sh
@@ -2,5 +2,6 @@
 # python ./wandb_result.py --metric val-aux search 'eval_phimm'
 # python ./wandb_result.py --metric val-aux search 'grpo_prod_fy22_7b_bs64_rep_gt_t12_tis5_2k|dapo_prod_fy22_7b_rare05_gt_t12_tis5_n8_wer_20_2k|grpo_tts_entity_7b_gt_t12_tis5_n16_2k_mw10|grpo_tts_entity_sr_fy22_7b_gt_t12_tis5_n16_2k_mw10'
 # python ./wandb_result.py --metric val-aux search '[dapo|grpo]_prod_fy22_7b_rare01' 2198
-python ./wandb_result.py --metric val-aux search 'entity' 2198
+# python ./wandb_result.py --metric val-aux search 'entity' 2198
+python ./wandb_result.py --metric val-aux search eval_entity_v0_7b
 
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -34,7 +34,8 @@
 
 # bash submit_job.sh h-n2-uks7 dapo_tts_entity_7b_gt_t12_tis5_n16_2k_mw10 true
 # bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_bs128_t06
-bash submit_job.sh h-n2-hpe4 grpo_gpt_entity_v0_7b_n4_2k_remax_t06
+# bash submit_job.sh h-n2-hpe4 grpo_gpt_entity_v0_7b_n4_2k_remax_t06
+# bash submit_job.sh h-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_e1_remax
 
 # bash submit_job.sh h1-n2-hpe4 grpo_7b_hc_rare01_n8_mw05_e1_remax_n2
 # bash submit_job.sh l-n1-hpe2     grpo_gpt_entity_v0_7b_n4_2k_remax
@@ -45,3 +46,5 @@ bash submit_job.sh h-n2-hpe4 grpo_gpt_entity_v0_7b_n4_2k_remax_t06
 # bash submit_job.sh h-n2-hpe4 grpo_gpt_entity_v0_7b_n2x8_2k_remax true
 # bash submit_job.sh h-n2-hpe4 eval_entity_v0_7b_remax_2k
 # bash submit_job.sh h-n2-uks7 eval_entity_v0_7b
+# bash submit_job.sh h-n2-uks7 eval_entity_7b
+bash submit_job.sh h1-n2-wus2 eval_entity_v0_7b_remax_2k
