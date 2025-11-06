b64c2ceff9c88811309850da10db403ab73514ef
Bo Ren <boren@microsoft.com>
2025-11-06 18:23:38 +0000

    update jobs

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -43,10 +43,11 @@
 # bash submit_job.sh h-n2-hpe4 grpo_gpt_entity_v0_7b_n2x8_2k_remax true
 # bash submit_job.sh h-n2-uks7 eval_entity_v0_7b
 # bash submit_job.sh h-n2-uks7 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_asr
-bash submit_job.sh h1-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_nogt
+# bash submit_job.sh h1-n2-wus2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_nogt
 # bash submit_job.sh l-n1-uks7 eval_entity_v0_7b_remax_2k
 # bash submit_job.sh dev-n1-wus2 eval_entity_v0_7b
 # bash submit_job.sh dev-n1-wus2 eval_entity_v0_7b_remax_2k
 # bash submit_job.sh l1-n1-hpe2 grpo_prod_fy22_7b_rare01_n8_mw05_2k_remax_fp16
 # bash submit_job.sh h-n2-uks7 grpo_gpt_entity_v0_7b_n2x8_2k_remax
 # bash submit_job.sh h-n2-hpe4 grpo_prod_fy22_7b_rare01_n8_mw05_e1_remax
+bash submit_job.sh h-n2-hpe4 grpo_prod_fy23_7b_rare01_n8_mw05_e1_remax
