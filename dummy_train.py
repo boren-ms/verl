31192402662b9e35a78ec97c1e4bc8d79d812932
Bo Ren <boren@microsoft.com>
2025-12-18 17:49:52 +0000

    update dummy train

--- dummy_train.py
+++ dummy_train.py
@@ -1,5 +1,6 @@
 import fire
 import time
+import os
 
 
 def dummy_training_loop():
@@ -9,6 +10,8 @@ def dummy_training_loop():
     while True:
         step += 1
         print(f"Training step {step}")
+        for key, value in os.environ.items():
+            print(f"{key}= {value}")
         time.sleep(1)
 
 
