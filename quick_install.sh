67461cb7ee73928ff4472b99f95e740ae9570499
Bo Ren <boren@microsoft.com>
2025-12-26 21:45:18 +0000

    update script

---
+++ quick_install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -xeuo pipefail
+done_file=".env_done"
+
+running_file=".env_running"
+
+echo "[INFO] Checking environment installation status..."
+
+while [ -f ${running_file} ]; do
+    echo "[INFO] Waiting for the installation to complete..."
+    sleep 10
+done
+
+if [ ! -f ${done_file} ]; then
+    touch ${running_file}
+    echo "[INFO] Installing environment..."
+    pip install -r requirements_vllm.txt
+    pip install --no-deps -e .
+    pip install  flash-attn==2.7.4.post1
+    apt install lsof
+    mv ${running_file} ${done_file}
+else
+    echo "[INFO] Environment already installed, skipping installation."
+fi
---
+++ recipe/one_step_off_policy/sync_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Syncing data from Azure Blob Storage to local machine"
+bbb sync az://orngwus2cresco/data/boren/data/gsm8k/ ~/data/gsm8k/
+bbb sync az://orngwus2cresco/data/boren/data/ckp/hf_models/Qwen2.5-Math-7B/ ~/data/ckp/hf_models/Qwen2.5-Math-7B/
+bbb sync az://orngwus2cresco/data/boren/data/ckp/hf_models/Qwen2.5-0.5B-Instruct/ ~/data/ckp/hf_models/Qwen2.5-0.5B-Instruct/
