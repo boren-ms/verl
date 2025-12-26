f7014d8fab1541b01982f3b92904af6a21568c68
Bo Ren <boren@microsoft.com>
2025-12-26 22:58:40 +0000

    update sync

--- recipe/one_step_off_policy/shell/sync_data.sh
+++ recipe/one_step_off_policy/shell/sync_data.sh
@@ -3,3 +3,4 @@ echo "Syncing data from Azure Blob Storage to local machine"
 bbb sync az://orngwus2cresco/data/boren/data/gsm8k/ ~/data/gsm8k/
 bbb sync az://orngwus2cresco/data/boren/data/ckp/hf_models/Qwen2.5-Math-7B/ ~/data/ckp/hf_models/Qwen2.5-Math-7B/
 bbb sync az://orngwus2cresco/data/boren/data/ckp/hf_models/Qwen2.5-0.5B-Instruct/ ~/data/ckp/hf_models/Qwen2.5-0.5B-Instruct/
+bbb sync az://orngwus2cresco/data/boren/data/ckp/hf_models/Qwen3-0.6B/ ~/data/ckp/hf_models/Qwen3-0.6B/
