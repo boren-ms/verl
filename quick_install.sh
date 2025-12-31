496b8f50815f4ef435228ce9593325e64125e1bf
Bo Ren <boren@microsoft.com>
2025-12-31 22:09:12 +0000

    update quick install

--- quick_install.sh
+++ quick_install.sh
@@ -11,6 +11,12 @@ while [ -f ${running_file} ]; do
     sleep 10
 done
 
+storage_account="orngwus2cresco"
+rel_paths=(
+    data/gsm8k
+    data/ckp/hf_models/Qwen2.5-0.5B-Instruct
+)
+
 if [ ! -f ${done_file} ]; then
     touch ${running_file}
     echo "[INFO] Installing environment..."
@@ -20,6 +26,11 @@ if [ ! -f ${done_file} ]; then
     echo "Restart Ray server with new version"
     ray stop
     ray start --head
+    
+    echo "[INFO] Preparing data ..."    
+    for rel_path in "${rel_paths[@]}"; do
+        bbb sync az://${storage_account}/data/boren/"${rel_path}"/ /root/"${rel_path}"/
+    done
     mv ${running_file} ${done_file}
 else
     echo "[INFO] Environment already installed, skipping installation."
