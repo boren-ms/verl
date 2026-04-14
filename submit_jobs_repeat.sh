82d611efe54078b4ddeb4d09094afa2ed7586297
Bo Ren <boren@microsoft.com>
2026-04-14 21:35:18 +0000

    fix job

--- submit_jobs_repeat.sh
+++ submit_jobs_repeat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #bash submit_job.sh verl-n1-i0  grpo_gpt_entity_v1a_7b_n4_1e_remax_lr05 # example
-bash submit_job.sh verl-n1-i0  grpo_repeat_ls_raw_wer_gt20_7b_n4_1e_remax_lr05  # grpo_hallucination
+bash submit_job.sh verl-n1-i0  grpo_repeat_remax_ls_wer02_n4_remax_lr05_s100  # grpo_hallucination
