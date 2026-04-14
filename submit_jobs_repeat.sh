8ef0068433850ead4f4191456c43c3e9adf84277
Bo Ren <boren@microsoft.com>
2026-04-14 21:08:13 +0000

    add 1st repeat examples

---
+++ submit_jobs_repeat.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#bash submit_job.sh verl-n1-i0  grpo_gpt_entity_v1a_7b_n4_1e_remax_lr05 # example
+bash submit_job.sh verl-n1-i0  grpo_repeat_ls_raw_wer_gt20_7b_n4_1e_remax_lr05  # grpo_hallucination
