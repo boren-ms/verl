6838e94a0092f46442fda82a4f45aad2eed0afde
Bo Ren <boren@microsoft.com>
2026-06-26 17:21:44 +0000

    refine qwen audio package, and register

--- plugins/qwen35_audio/hf_model/__init__.py
+++
@@ -1,3 +0,0 @@
-# Self-contained Qwen3.5 Audio model for HF trust_remote_code deployment.
-from .configuration_qwen3_5_audio import Qwen3_5AudioConfig
-from .modeling_qwen3_5_audio import Qwen3_5AudioForCausalLM
