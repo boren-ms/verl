15d986ad8f46b8f42c18fe0553f9fa92175923e3
Bo Ren <boren@microsoft.com>
2026-06-26 18:34:51 +0000

    use VERL_USE_EXTERNAL_MODULES

--- recipe/phimm/asr_rollout.py
+++ recipe/phimm/asr_rollout.py
@@ -277,6 +277,7 @@ def init_ray(config):
     runtime_env = {
         "env_vars": {"TOKENIZERS_PARALLELISM": "true", "NCCL_DEBUG": "WARN",
                      "VLLM_LOGGING_LEVEL": "WARN", "HF_HUB_OFFLINE": "1",
+                     "VERL_USE_EXTERNAL_MODULES": "hf_qwen35_audio",
                      "PYTORCH_ALLOC_CONF": "expandable_segments:True", **env_vars},
         "excludes": [str(Path(__file__).parents[2] / ".git")],
         **ray_init_kwargs.pop("runtime_env", {}),
