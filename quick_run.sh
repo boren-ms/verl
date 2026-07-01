e4e62ed4e83c133888db5db09d66e6ff96a3525a
Bo Ren <boren@microsoft.com>
2026-07-01 22:50:40 +0000

    add gmpo

--- quick_run.sh
+++ quick_run.sh
@@ -23,6 +23,8 @@ elif [[ "$config_name" == remax_* ]]; then
     module="recipe.phimm.main_asr_remax"
 elif [[ "$config_name" == grpo_* ]]; then
     module="recipe.phimm.main_asr_grpo"
+elif [[ "$config_name" == gmpo_* ]]; then
+    module="recipe.phimm.main_asr_grpo"
 elif [[ "$config_name" == rloo_* ]]; then
     module="recipe.phimm.main_asr_rloo"
 fi
