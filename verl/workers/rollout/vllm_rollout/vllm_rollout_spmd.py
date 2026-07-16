2fd01bddad87823bb9f520a5a9ce52ede778ab31
Bo Ren <boren@microsoft.com>
2026-07-16 21:41:27 +0000

    fix gt response

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -408,7 +408,7 @@ class vLLMRollout(BaseRollout):
                 if i % self.config.n == 0 and self.config.gt_rollout and not is_validate:
                     reward_i = non_tensor_batch["reward_model"][i]
                     gt_text = reward_i.get("gt_output", reward_i.get("ground_truth", ""))
-                    response[i] = self.tokenizer.encode(gt_text + "<|end|>")
+                    response[i] = self.tokenizer.encode(gt_text) + [self.tokenizer.eos_token_id]
 
             response = pad_2d_list_to_length(response, self.pad_token_id, max_length=self.config.response_length).to(
                 idx.device
